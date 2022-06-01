6f36d11b7e1245dfddc17cd5642cec73b6ee1318
Sean Martin <sean@seanmartinstudio.com>
2022-06-01 15:27:53 -0700

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 1, "M", "Brown", "Happy", 0),
+("Rowdy", 2, "F", "Black", "Sad", 0),
+("Tabitha", 3, "F", "Brown", "Happy", 0),
+("Sergeant Brown", 4, "M", "Black", "Sad", 0),
+("Melissa", 5, "F", "Brown", "Happy", 0),
+("Grinch", 6, "M", "Black", "Sad", 0),
+("Wendy", 7, "M", "Brown", "Happy", 0),
+(NULL, 8, "F", "Black", "Sad", 0)
